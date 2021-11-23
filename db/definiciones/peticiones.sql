f4ad6bf36baaf7473136cc5350ee196541f58a5e
DanielMontesGuerrero <danimontesguerrero17@gmail.com>
2021-11-22 20:08:22 -0600

    Configuración del linter

--- db/definiciones/peticiones.sql
+++ db/definiciones/peticiones.sql
@@ -1,6 +1,7 @@
 CREATE TABLE peticiones (
 	id_peticion BIGINT(20) NOT NULL AUTO_INCREMENT,
 	id_usuario BIGINT(20) NOT NULL,
+	comprobante VARCHAR(255) NOT NULL,
 	estado ENUM('pendiente', 'rechazado', 'aceptado') NOT NULL DEFAULT 'pendiente',
 	PRIMARY KEY (id_peticion, id_usuario)
 );
