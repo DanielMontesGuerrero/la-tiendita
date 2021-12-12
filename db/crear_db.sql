b3f1ed94ae44ffa5d9cce667e3d2ab7ecac210f7
DanielMontesGuerrero <danimontesguerrero17@gmail.com>
2021-12-12 16:10:56 -0600

    Añadir operaciones CRUD y endpoints para las escuelas

--- db/crear_db.sql
+++ db/crear_db.sql
@@ -49,7 +49,7 @@ CREATE TABLE entregas_en (
 CREATE TABLE instituciones (
 	id_institution BIGINT(20) NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(50) NOT NULL,
-	logo VARCHAR(255),
+	image VARCHAR(255),
 	PRIMARY KEY (id_institution)
 );
 CREATE TABLE metodos_de_pago (
