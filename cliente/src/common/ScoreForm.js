4b5971a28f6925505e9de2b63217ca8fac13780e
DanielMontesGuerrero <danimontesguerrero17@gmail.com>
2021-12-13 17:55:00 -0600

    Añadir funcionalidad para enviar reseñas de los productos

---
+++ cliente/src/common/ScoreForm.js
@@ -0,0 +1,117 @@
+import React, {Component} from 'react';
+import {Form, Modal, Button, FloatingLabel} from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import UserProfile from '../common/UserProfile.js';
+import config from '../common/config.js';
+import axios from 'axios';
+
+class ScoreForm extends Component {
+  constructor(props) {
+    super(props);
+    this.score = 0;
+    this.description = '';
+  }
+
+  static get propTypes() {
+    return {
+      scoreType: PropTypes.string,
+      id_store: PropTypes.number,
+      id_product: PropTypes.number,
+      show: PropTypes.bool,
+      onHide: PropTypes.func,
+      name: PropTypes.string,
+    };
+  }
+
+  sendScore() {
+    const isValidScore = (score) => {
+      if (!/\d+/.test(score)) {
+        return false;
+      }
+      score = parseFloat(score);
+      if (score < 0 || score > 5) {
+        return false;
+      }
+      return true;
+    };
+    if (!isValidScore(this.score) ||
+      !(this.props.scoreType === 'product' ||
+        this.props.scoreType === 'store')) {
+      alert('La calificación debe estar en el rango [0,5]');
+    } else if (UserProfile.getIdUser() === null) {
+      alert('Necesitas iniciar sesión');
+    } else {
+      const id = this.props.scoreType === 'store' ?
+        this.props.id_store : this.props.id_product;
+      const options = {
+        url: `${config.host}/${this.props.scoreType}/score/${id}`,
+        method: 'post',
+        data: {
+          id_user: UserProfile.getIdUser(),
+          score: parseFloat(this.score),
+          description: this.description,
+        },
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
+        },
+      };
+      axios(options).then((res) => {
+        console.log(res);
+        if (res.data.response) {
+          alert('Calificación enviada');
+          this.props.onHide();
+        }
+      });
+    }
+  }
+
+  render() {
+    return (
+      <Modal
+        {...this.props}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Enviar nueva reseña</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <p>Hola, tus comentarios ayudan a la comunidad a recibir
+            un servicio de calidad
+          </p>
+          <p>Ingresa tu reseña de: <b>{this.props.name}</b></p>
+          <FloatingLabel
+            controlId="score"
+            label="Calificación"
+            className="mb-2"
+          >
+            <Form.Control
+              type="number"
+              onChange={(evt) => this.score = evt.target.value}
+            />
+          </FloatingLabel>
+          <FloatingLabel controlId="comments" label="Reseña">
+            <Form.Control
+              as="textarea"
+              placeholder="Ingresa tus comenta aquí"
+              style={{height: '100px'}}
+              onChange={(evt) => this.description = evt.target.value}
+            />
+          </FloatingLabel>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button
+            variant="secondary"
+            onClick={this.props.onHide}>
+            Cancelar
+          </Button>
+          <Button variant="primary" onClick={() => this.sendScore()}>
+            Enviar reseña
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
+
+export default ScoreForm;
