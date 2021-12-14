4081e86bcc9e82cb4470063ef7fabde8d03de7a2
DanielMontesGuerrero <danimontesguerrero17@gmail.com>
2021-12-13 23:00:45 -0600

    Correcciones en RegisterForm

--- db/definiciones/usuarios.sql
+++ db/definiciones/usuarios.sql
@@ -3,7 +3,7 @@ CREATE TABLE usuarios (
 	`name` VARCHAR(30) NOT NULL UNIQUE,
 	email VARCHAR(30) NOT NULL UNIQUE,
 	`image` VARCHAR(255),
-	id_institution BIGINT(20),
+	id_institution BIGINT(20) NULL,
 	userType ENUM('admin', 'vendendor', 'usuario') NOT NULL DEFAULT 'usuario',
 	password VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id_user, name)
