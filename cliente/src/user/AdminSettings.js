bc762df22f30e23ebbad287e1755af22f4b2be64
zanzakigus <erz_ra@hotmail.com>
2021-12-19 22:49:28 -0600

    Front solictud para ser vendedor, Admin manejo de las peticiones, aceptacion: cambiar status peticion, cambiar tipo de usuario y crear una tienda vacia

--- cliente/src/user/AdminSettings.js
+++ cliente/src/user/AdminSettings.js
@@ -1,23 +1,54 @@
 import React, {Component} from 'react';
 import {Stack, Card} from 'react-bootstrap';
-import Request from './Request.js';
+import config from '../common/config';
+import axios from 'axios';
+import Request from './Request';
 
 class AdminSettings extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      requests: [],
+    };
+  }
+
+  async componentDidMount() {
+    /* let request = [];*/
+    const options = {
+      url: `${config.host}/user/get_all_requests`,
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+    };
+    axios(options).then(async (res) => {
+      const requests = res.data.response;
+      const len = requests.length;
+      for (let i = 0; i < len; i++) {
+        options.url = `${config.host}/user/${requests[i].id_user}`;
+        await axios(options).then((resP) => {
+          console.log(resP.data.response);
+          requests[i].user = resP.data.response.name;
+        });
+        options.url = `${config.host}/user/request/${requests[i].id_petition}`;
+        await axios(options).then((resP) => {
+          console.log(resP.data.response);
+          requests[i].downloadLink = resP.data.response;
+        });
+      }
+      this.setState({requests: requests});
+      console.log(this.state.requests);
+    });
+  }
   getRequests() {
-    const request = [
-      {
-        user: 'Fulano',
-        downloadLink: '#',
-      },
-      {
-        user: 'Daniel',
-        downloadLink: '#',
-      },
-    ];
+    const request = this.state.requests;
     return request.map((item, index) => {
       return (<Request
         user={item.user}
         downloadLink={item.downloadLink}
+        id_user={item.id_user}
+        voucher={item.voucher}
+        id ={item.id_petition}
         key={index}/>);
     });
   }
--- cliente/src/user/Request.js
+++ cliente/src/user/Request.js
@@ -1,13 +1,73 @@
 import React, {Component} from 'react';
 import {Card, Button, Stack} from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import config from '../common/config';
+import axios from 'axios';
 
 class Request extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: false,
+    };
+  }
   static get propTypes() {
     return {
+      id: PropTypes.number,
+      id_user: PropTypes.number,
+      voucher: PropTypes.text,
       user: PropTypes.text,
       downloadLink: PropTypes.text,
     };
+  }
+
+  aceptarSolicitud() {
+    const options = {
+      url: `${config.host}/user/request/${this.props.id}`,
+      method: 'patch',
+      data: {
+        state: 'aceptado'},
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+    };
+    axios(options).then((res) => {
+      options.url = `${config.host}/user/${this.props.id}`;
+      options.data = {
+        userType: 'vendendor',
+      };
+      console.log(res);
+      axios(options).then((resU) => {
+        console.log(resU);
+        options.url = `${config.host}/store`;
+        options.data = {
+          name: `Tienda de ${this.props.user}`,
+          description: 'Modifica tu descripcion',
+          id_user: this.props.id_user,
+        };
+        options.method = 'post';
+        axios(options).then((resT) => {
+          this.setState({status: true});
+          alert('Se ha aceptado la apeticion y se a creado una tienda');
+        });
+      });
+    });
+  }
+
+  rechazarSolicitud() {
+    const options = {
+      url: `${config.host}/user/request/${this.props.id}`,
+      method: 'patch',
+      data: {
+        state: 'rechazado'},
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+    };
+    axios(options).then((res) => {
+      this.setState({status: true});
+      alert('La informacion ha sido actualizada');
+    });
   }
 
   render() {
@@ -21,8 +81,10 @@ class Request extends Component {
             </a>
           </Card.Text>
           <Stack gap={2} direction="horizontal">
-            <Button variant="success">Aceptar</Button>
-            <Button variant="danger">Rechazar</Button>
+            {/* eslint-disable-next-line max-len */}
+            <Button disabled={this.state.status} variant="success" onClick={()=>this.aceptarSolicitud()}>Aceptar</Button>
+            {/* eslint-disable-next-line max-len */}
+            <Button disabled={this.state.status} variant="danger" onClick={()=>this.rechazarSolicitud()}>Rechazar</Button>
           </Stack>
         </Card.Body>
       </Card>
